1dfc49d6fece73e56d3ea5d74b9cd7c62c5a5d43
gauav verma <gauravverma64786@gmail.com>
2021-09-29 15:35:57 +0530

    dd

--- main.py
+++ main.py
@@ -1,36 +1,37 @@
-from flask import url_for, redirect, Flask, render_template, request, jsonify
+from flask import url_for, redirect, Flask, render_template, request, jsonify, flash
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, login_user, login_required
+from flask_login import LoginManager, UserMixin, \
+    login_required, login_user, logout_user
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
 db = SQLAlchemy(app)
 
+app.config.update(
+    DEBUG=True,
+    SECRET_KEY='secret_xxx'
+)
+app.config['TESTING'] = False
+# flask-login
 login_manager = LoginManager()
 login_manager.init_app(app)
+login_manager.login_view = "/login"
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
+    """ Create user table"""
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, unique=True, nullable=False)
-    password = db.Column(db.String, unique=False, nullable=False)
+    username = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(80))
 
     def __init__(self, username, password):
         self.username = username
         self.password = password
 
-    def to_json(self):
-        return {"name": self.name,
-                "email": self.email}
 
-    def is_authenticated(self):
-        return True
-
-    def is_active(self):
-        return True
-
-    def get_id(self):
-        return str(self.id)
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.filter_by(id=user_id).first()
 
 
 @app.route('/')
@@ -42,11 +43,19 @@ def home_page():
 def login_page():
     if request.method == "POST":
         data = request.form.to_dict()
-        user = db.session.query(db.exists().where(User.username == 'admin')).scalar()
-        print(user)
-        if user:
-            login_user(user)
-            return f"logged in {data['username']} with {data['password']}"
+        if "" not in list(data.values()):
+            user = User.query.filter_by(username=data['username'], password=data['password']).first()
+            if user is not None:
+                print(user)
+                login_user(user)
+                print(user.is_active)
+                return redirect("/send_complaint")
+            else:
+                flash("Incorrect Username or Password")
+                return redirect("/login")
+        else:
+            flash("Fields Shouldn't Be empty")
+            return redirect("/login")
     else:
         return render_template("login.html")
 
@@ -54,7 +63,13 @@ def login_page():
 @app.route("/send_complaint")
 @login_required
 def complaint_page():
+    flash("You are loggrd in", "information")
     return "Secure"
+
+
+@login_manager.unauthorized_handler
+def unauthorized_callback():
+    return redirect('/login')
 
 
 if __name__ == '__main__':
